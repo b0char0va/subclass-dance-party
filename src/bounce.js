9bb6a7cd87d1cea418a2945cf78121f28d9f3635
Mona Bocharova <bocharova.mona@gmail.com>
2018-09-25 22:40:35 -0700

    fixed a bug

--- src/bounce.js
+++ src/bounce.js
@@ -1,7 +1,8 @@
 var BouncingDancer = function(top, left, timeBetweenSteps, color, border) {
   Dancer.call(this, top, left, timeBetweenSteps, color, border);
   this.$node.addClass('bounce');
-  $(this.$node).append('<img src="bounce.gif" class="bouncey">');
+  this.counter = 2;
+  $(this.$node).append('<img src="bounce.gif">');
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -17,5 +18,11 @@ BouncingDancer.prototype.step = function() {
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  // this.$node.addClass('bouncey');
+    if (this.counter % 2 === 0) {
+        this.$node.animate({top: this.top + '50px'});
+        this.counter++;
+    } else {
+        this.$node.animate({top: '-=50px'});
+        this.counter++;
+    }
 };
